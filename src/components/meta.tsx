951e1c98d5c8f96ea393b5cdd7eccd492b5fa32c
OdenCraftsman <oden13123@gmail.com>
2023-02-09 05:49:41 +0900

    asd

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -6,7 +6,7 @@ export default function Meta({title}:{title:string}) {
             <title>{title}</title>
             <meta name="description" content="Storage of my technology" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/dracula.min.css"></link>
+            {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/dracula.min.css"></link> */}
         </Head>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -51,9 +51,9 @@ function App({ Component, pageProps }: AppProps){
             <main>
                 <Component {...pageProps} />
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js"></script>
-                <script>
+                {/* <script>
                     hljs.initHighlightingOnLoad();
-                </script>
+                </script> */}
             </main>
             <Footer fixed={isIndex}/>
         </>
